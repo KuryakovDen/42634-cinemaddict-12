e66c608e915318a1d12d594beade2931a303af80
Fifer2 <KuryakovDenis@yandex.ru>
2020-07-28 16:18:09 +0700

    feat: added filtersComponent

--- src/main.js
+++ src/main.js
@@ -9,11 +9,19 @@ const renderUserRankComponent = () => {
   `;
 };
 
-// const renderMenuComponent = () => {
-//   return `
-//     <p>Denis</p>
-//   `;
-// };
+const renderFiltersComponent = () => {
+  return `
+  <nav class="main-navigation">
+    <div class="main-navigation__items">
+      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+    </div>
+    <a href="#stats" class="main-navigation__additional">Stats</a>
+  </nav>
+  `;
+};
 
 const renderComponent = (container, place = `beforeend`, template) => {
   container.insertAdjacentHTML(place, template);
@@ -23,4 +31,4 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
 
 renderComponent(siteHeaderElement, `beforeend`, renderUserRankComponent());
-// renderComponent(siteMainElement, `beforeend`, renderMenuComponent());
+renderComponent(siteMainElement, `beforeend`, renderFiltersComponent());
