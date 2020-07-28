5e7905da7f993cd9f0ddee74341f41179c6156ca
Fifer2 <KuryakovDenis@yandex.ru>
2020-07-28 19:18:01 +0700

    feat: added statisticsComponent

--- src/main.js
+++ src/main.js
@@ -71,12 +71,21 @@ const renderShowMoreCompnent = () => {
   `;
 };
 
+const renderStatisticsComponent = () => {
+  return `
+  <section class="footer__statistics">
+    <p>130 291 movies inside</p>
+  </section>
+  `;
+};
+
 const renderComponent = (container, place = `beforeend`, template) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
+const siteFooterElement = document.querySelector(`.footer`);
 
 
 renderComponent(siteHeaderElement, `beforeend`, renderUserRankComponent());
@@ -84,11 +93,13 @@ renderComponent(siteMainElement, `beforeend`, renderFiltersComponent());
 renderComponent(siteMainElement, `beforeend`, renderSortComponent());
 renderComponent(siteMainElement, `beforeend`, renderFilmBoardComponent());
 
-const siteFilmsContainer = document.querySelector(`.films-list__container`);
+const siteFilmsContainer = siteMainElement.querySelector(`.films-list__container`);
 
 for (let i = 0; i < COUNT_OF_FILM_CARDS; i++) {
   renderComponent(siteFilmsContainer, `beforeend`, renderFilmCardComponent());
 }
 
 renderComponent(siteMainElement, `beforeend`, renderShowMoreCompnent());
+renderComponent(siteFooterElement, `beforeend`, renderStatisticsComponent());
+
 
