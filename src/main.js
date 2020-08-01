3082710ade7f1740be05799cc55b4984f803045e
Fifer2 <KuryakovDenis@yandex.ru>
2020-08-01 17:19:46 +0700

    refactor: move user-rank component to separate file

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-'use strict';
+import {renderUserRankComponent} from './view/user-rank.js';
 
 const COUNT_OF_FILM_CARDS = 5;
-
-const renderUserRankComponent = () => {
-  return `
-  <section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
-    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-  </section>
-  `;
-};
 
 const renderFiltersComponent = () => {
   return `
